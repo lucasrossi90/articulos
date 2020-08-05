cc37b1b21ac4fa6c690db4b35168cea998fba986
programando <lucasrossi90@gmail.com>
2020-08-05 05:50:26 -0700

    modified migration

--- db/migrate/20200624152641_remove_columns_producto_movido.rb
+++ db/migrate/20200624152641_remove_columns_producto_movido.rb
@@ -1,6 +1,5 @@
 class RemoveColumnsProductoMovido < ActiveRecord::Migration[5.2]
   def change
-  	remove_column :producto_movidos, :fecha
   	remove_column :producto_movidos, :articulos_id
   end
 end
